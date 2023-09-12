3675995fe17d24165117b2d413a851bf3fe3eda3
erik <eblillehagen@gmail.com>
2023-09-12 13:37:36 +0200

    add missing files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'mc-white': '#F4F3EE',
+        'mc-black': '#0B0C11'
+      }
+    },
+  },
+  plugins: [],
+}
+
