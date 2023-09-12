b7c6da57d18313d87935ba534ccd15a52e070cba
erik <eblillehagen@gmail.com>
2023-09-12 13:22:32 +0200

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,13 @@
 	<script async src="https://cse.google.com/cse.js?cx=4057f0107f2b1450f"></script>
 </svelte:head>
 
-<img alt="The project logo" src={logo} />
-<div class="gcse-search" />
+<div class="bg-mc-white absolute w-full h-full flex flex-col">
+	<div class="flex justify-center py-20">
+		<img class="w-1/3" alt="Modesty Concept logo" src={logo} />
+	</div>
+	<div class="flex justify-center">
+		<div class="w-9/12">
+			<div class="gcse-search" />
+		</div>
+	</div>
+</div>
