5e6fb459bd536d8a777e10ac52a6d4ee21b252d6
erik <eblillehagen@gmail.com>
2023-09-12 12:20:04 +0200

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+	import logo from '$lib/assets/logo.png';
+</script>
+
 <svelte:head>
 	<script async src="https://cse.google.com/cse.js?cx=4057f0107f2b1450f"></script>
 </svelte:head>
 
+<img alt="The project logo" src={logo} />
 <div class="gcse-search" />
