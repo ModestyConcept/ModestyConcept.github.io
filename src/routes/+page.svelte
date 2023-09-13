9126e3dd2c23c0efa940764335b39c23a1d542b9
erik <eblillehagen@gmail.com>
2023-09-13 11:04:09 +0200

    fix css issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,13 @@
 	<script async src="https://cse.google.com/cse.js?cx=4057f0107f2b1450f"></script>
 </svelte:head>
 
-<div class="bg-mc-white absolute w-full h-full flex flex-col">
-	<div class="flex justify-center py-20">
-		<img class="w-1/3" alt="Modesty Concept logo" src={logo} />
+<body class="bg-mc-white absolute w-screen flex flex-col">
+	<div class="flex justify-center py-3 md:py-20">
+		<img class="w-2/3 md:w-1/3" alt="Modesty Concept logo" src={logo} />
 	</div>
 	<div class="flex justify-center">
-		<div class="w-9/12">
+		<div class="w-11/12 md:w-9/12">
 			<div class="gcse-search" />
 		</div>
 	</div>
-</div>
+</body>
